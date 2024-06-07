b795b7f0b9afa3d740cf3414bd4d158a87f5efd4
EmilyG101 <101006556+EmilyG101@users.noreply.github.com>
2024-06-07 10:43:49 +0000

    Added code to find max and low

--- Main.java
+++ Main.java
@@ -23,7 +23,7 @@ public class Main {
     }
 
     // make sure all data entered the ArrayList
-    //System.out.println(totalScore);
+    System.out.println(totalScore);
     System.out.println(totalScore.size() + " students took the exam!");
     int total =0;
     for (int score : totalScore){
@@ -31,6 +31,28 @@ public class Main {
     }
     double average = (double)total/totalScore.size();
     System.out.println("The average score is " + average);
+
+    // find the highest score\
+    int high = 0;
+    int num = 0;
+    for (int score : totalScore){
+      num = score;
+      if(num > high){
+        high = num;
+      }
+    }
+    System.out.println("The highest score is " + high);
+
+    // find the lowest score\
+    int low = 0;
+    int num1 = 0;
+    for(int score : totalScore){
+      num1 = score;
+      if(num1 < low){ 
+        low = num1;
+      }
+    }
+    System.out.println("The lowest score is " + low);
   }
 
 }
